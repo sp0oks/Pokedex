092749cfbd67ef654e0812a8348b8f01277fb6c8
Gabriel <g4briel.4lves@gmail.com>
2019-11-26 17:33:35 -0300

    Begin front-end development

---
+++ vue-frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <Navbar/>
+        <h2>Pokedex</h2>
+    </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar';
+
+import '..node_modules/materialize-css/dist/js/materialize.min.js';
+import '..node_modules/materialize-css/dist/css/materialize.min.css';
+
+export default {
+  name: 'app',
+  components: {
+    Navbar
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ vue-frontend/src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+    <nav>
+    <div class="nav-wrapper">
+        <a href="#" class="brand-logo">Pokedex</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Find Poke</a></li>
+        </ul>
+    </div>
+    </nav>
+</template>
